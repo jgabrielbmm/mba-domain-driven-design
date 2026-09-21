e525ea30baed7e483c173581107d1268aeb1e413
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2026-09-21 20:00:10 -0300

    feat(emails): consumir notificacao da lista de espera

--- apps/emails/src/consumer.service.ts
+++ apps/emails/src/consumer.service.ts
@@ -5,6 +5,29 @@ import { Injectable } from '@nestjs/common';
 export class ConsumerService {
   @RabbitSubscribe({
     exchange: 'amq.direct',
+    routingKey: 'SpotOfferedToWaitingCustomerIntegrationEvent',
+    queue: 'emails.waiting-list',
+  })
+  handleWaitingCustomer(msg: {
+    event_name: string;
+    payload: {
+      customer_id: string;
+      event_id: string;
+      section_id: string;
+      spot_id: string;
+    };
+  }) {
+    console.log('ConsumerService.handleWaitingCustomer', {
+      event_name: msg.event_name,
+      customer_id: msg.payload.customer_id,
+      event_id: msg.payload.event_id,
+      section_id: msg.payload.section_id,
+      spot_id: msg.payload.spot_id,
+    });
+  }
+
+  @RabbitSubscribe({
+    exchange: 'amq.direct',
     routingKey: 'PartnerCreatedIntegrationEvent',
     //routingKey: 'events.fullcycle.com/*',
     queue: 'emails',
